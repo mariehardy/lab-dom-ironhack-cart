5162e331c1a03a848a5405d508ce9399cf9171cd
marie-pascale hardy <mariepascalehardy@marie-pascales-MacBook-Pro.local>
2020-02-18 21:36:59 +0100

    BER WDPT Marie

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,30 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
-function updateSubtot($product) {
+let product = document.querySelectorAll('.product')
+
+function updateSubtot(product) {
   // Iteration 1.1
+  let priceUnit = document.getElementsByTagName('span')[1].innerText;
+  let productQuantity = document.querySelectorAll('input')[0].value;
+  console.log(priceUnit)
+
+  return priceUnit * productQuantity
 }
 
 function calcAll() {
   // Iteration 1.2
+
+  //for (let i=0;i<2;i++) {
+
+  let subTotal = document.getElementsByTagName('span')[0];
+
+
+  subTotal.innerText = updateSubtot(product)
+  console.log(subTotal)
+
+  //add up all subTotals
+  //return in Total
+  //}
 }
 $calc.onclick = calcAll;
