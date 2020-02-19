c9fee18f6637127b0451c676d7d3077f533f2ee4
marie-pascale hardy <mariepascalehardy@mariepacalesmbp.localdomain>
2020-02-19 10:31:22 +0100

    BER WDPT Marie

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,30 +1,59 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
-let product = document.querySelectorAll('.product')
 
-function updateSubtot(product) {
+let $product = document.querySelectorAll('.product')[0]
+
+
+function updateSubtot($product) {
   // Iteration 1.1
-  let priceUnit = document.getElementsByTagName('span')[1].innerText;
-  let productQuantity = document.querySelectorAll('input')[0].value;
-  console.log(priceUnit)
+  let priceUnit = $product.querySelectorAll('.pu span')[0].innerText;
+  let quantity = $product.querySelectorAll('input')[0].value;
 
-  return priceUnit * productQuantity
+//  console.log(priceUnit)
+//  console.log(quantity)
+
+  return priceUnit * quantity
 }
 
-function calcAll() {
+//updateSubtot($product)
+
+
+function calcAll(event) {
   // Iteration 1.2
 
-  //for (let i=0;i<2;i++) {
+  //console.log("event.currentTarget", event.currentTarget)
 
-  let subTotal = document.getElementsByTagName('span')[0];
+  
+  let totalPrice = Number(document.querySelectorAll('h2 span')[0].innerText);
+  //console.log(typeof totalPrice)
+  //console.log(totalPrice)
 
 
-  subTotal.innerText = updateSubtot(product)
-  console.log(subTotal)
+//  for (let i=0; i < 2; i++) {
 
-  //add up all subTotals
-  //return in Total
-  //}
+  for (let i=0; i < document.querySelectorAll('.product')[0].length; i++) {
+
+
+    let $product = document.querySelectorAll('.product')[i];
+    console.log("product is " + $product)
+
+    let subTotal = $product.querySelectorAll('.pu span')[i].innerText;
+    console.log("subTotal is " + subTotal)
+    
+    subTotal = updateSubtot($product)
+    console.log("subTotal is now " + subTotal)
+
+      //add up all subTotals
+
+      totalPrice = subTotal
+      console.log("totalPrice returned is " + totalPrice)
+
+   
+  }
+
+
+
 }
 $calc.onclick = calcAll;
+
